315443c1f3964675e230f494de6e6c8de311f3db
sandy <chshangy@users.noreply.github.com>
2020-12-10 11:05:01 +0800

    Update Calculate-GI-HRD-Scores_from_ASCAT_segmentation.R

--- Calculate-GI-HRD-Scores_from_ASCAT_segmentation.R
+++ Calculate-GI-HRD-Scores_from_ASCAT_segmentation.R
@@ -4,23 +4,25 @@
 library(dplyr)
 library(magrittr)
 
-###HRD functions
-source("/home/rstudio/Jason_project/oncoscan_pipeline/HRD_calculation_Functions_multiple_methods_csy.R")
-source("/home/rstudio/Jason_project/oncoscan_pipeline/GI_calculation_Functions_multiple_methods_csy.R")
-source("/home/rstudio/Jason_project/oncoscan_pipeline/read_segmentation_Functions_csy.R")
+###load the functions
+setwd("/home/rstudio/oncoscan_pipeline") ##or the directory where the R files locate at
+source("HRD_calculation_Functions_multiple_methods_csy.R")
+source("GI_calculation_Functions_multiple_methods_csy.R")
+source("read_segmentation_Functions_csy.R")
   
 
 ##########set up the series to be processed:
 geo.series <- c("GSE125700", "GSE83916", "GSE80806", "GSE107394" )
-mainDir = "/home/rstudio/Jason_project/oncoscan_pipeline/data"
+mainDir = "/home/rstudio/oncoscan_pipeline/data" #or setup the your path
 
 #single gse
 #gse="GSE125700"
-#i=1
-##calculate the
+
+##calculate the HRD scores and GI scores for each sample of each gse series
 for(i in 1:length(geo.series)) {
   gse=geo.series[i]
   setwd(file.path(mainDir, gse))
+  #load the ASCAT segmentation file
   seg.cn <- read.delim(paste0("combined_ASCN_segmenation_", gse, ".txt", sep=""), header = T, stringsAsFactors = F, check.names = F)
   
   #Remove sex chromosomes
@@ -32,37 +34,36 @@ for(i in 1:length(geo.series)) {
   
   chrominfo <- GetChrominfo() # hg19
   
-  #get the segment as "TAI"
+  #get the 'TAI segment' location detail
   tai.seg <- calc.ai(seg.cn.hrd, chrominfo, return.loc = TRUE)
   tai.seg %<>% filter(AI==1) %>% select(SampleName, Chr, Start, End, AI)
   colnames(tai.seg)[which(names(tai.seg)=="AI")] <- "TAI"
-  #get tai value
+  #get tai segment count of each sample
   tai <- calc.ai(seg.cn.hrd, chrominfo)
   tai = data.frame(tai, check.names = F)
   tai %<>% select("Telomeric AI") %>% set_colnames("TAI") %>% mutate(SampleName = rownames(tai))
   
-  ######get the segment as LST
+  ######get the 'LST segment' location detail
   lst.seg <- calc.lst(seg.cn.hrd, chrominfo, return.loc = TRUE)
   lst.seg %<>% filter(LST==1) %>% distinct()
   lst.seg %<>% select(SampleName, Chr, Start, End, LST) %>% distinct()
-  ######Calculation of LST
+  ######calculate lst segment count of each sample
   lst <- calc.lst(seg.cn.hrd, chrominfo)
   lst <- data.frame(lst, check.names = F)
   lst %<>% set_colnames("LST") %>% mutate(SampleName = rownames(lst))
   
-  ##get the segment as LOH
+  ##get the 'LOH segment' location detail
   loh.seg <- calc.hrd(seg.cn.hrd, return.loc = TRUE)
   colnames(loh.seg)[which(names(loh.seg)=="HRD breakpoint")] <- "LOH"
   loh.seg %<>% select(SampleName, Chr, Start, End, LOH)
-  
-  ##Calculation of LOH HRD
+  ##calculate the loh segment count of each sample
   loh <- calc.hrd(seg.cn.hrd)
   loh <- data.frame(loh, check.names = F)
   loh %<>% set_colnames("LOH") %>% mutate(SampleName = rownames(loh))
   
-  ##prepare segmentation for calculation of GI scores
+  ##prepare segmentation for calculation of GI scores (GIS=Genomic instability, GID=Genomic index, GII=Genomic instability index)
   seg.cn$Log2Ratio <- as.numeric(as.character(seg.cn$Log2Ratio))
-  #define the CNA as numeric, gain = 1, loss=2, others=0
+  #define the CNA in terms of gain and loss as numeric, gain = 1, loss=2, others=0
   seg.cn.gi = seg.cn %>% mutate(CNA= sapply(Log2Ratio, function(x){ifelse(x>0.1, 1, ifelse(x < -0.1, 2, 0))}))
   seg.cn.gi %<>% left_join(tai.seg, by=c("SampleName", "Chr", "Start", "End")) %>% left_join(loh.seg, by=c("SampleName", "Chr", "Start", "End")) %>% left_join(lst.seg, by=c("SampleName", "Chr", "Start", "End"))  
   
